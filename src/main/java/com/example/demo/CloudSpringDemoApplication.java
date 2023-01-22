e517fa1ef30ac4e1fea9807ecdc59f66117b56f8
V Siva <velusamys@altimetrik.com>
2023-01-22 22:37:33 +0530

    FirstCommit

---
+++ src/main/java/com/example/demo/CloudSpringDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSpringDemoApplication.class, args);
+		System.out.println(" Server is Running...");
+	}
+
+}
